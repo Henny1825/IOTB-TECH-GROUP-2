ee1061b82b8c9070d6b2212180080c111a46e888
Henny1825 <mueezabdulazeez8@gmail.com>
2026-06-28 19:57:27 +0100

    feat: add fetch and search logic with error handling

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
 
+searchBtn.addEventListener('click', getInput);
+searchInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        getInput();
+    }
+});
+
+function getInput() {
+    const query = searchInput.value;
+    fetchData(query);
+}
+ 
+async function fetchData(query) {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+        const data = await response.json();
+        const meals = data.meals;
+
+        if (meals === null) {
+        return; 
+        } else {
+            return meals;  
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
